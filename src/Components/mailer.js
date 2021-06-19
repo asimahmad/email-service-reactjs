a8ae3dcf2595af1f179b6bf8746f8229fd9ee9c6
Asim <asim005011997@gmail.com>
2021-06-19 13:15:27 +0530

    Added the emailjs-com package to send the email, it is only for learning purpose

---
+++ src/Components/mailer.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import emailjs from 'emailjs-com'
+function mailer(props) {
+
+    function sendEmail(e){
+        e.preventDefault();
+        emailjs.sendForm('service_kvit3yf', 'template_qzge9ug', e.target, "user_XqqdUYeoKvlwYIq9AsKmK")
+        .then(res =>{
+            console.log(res)
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }
+    return (
+        <div className="container border" style={{marginTop:"50px", width:"50%", backgroundImage: `url('https://images.pexels.com/photos/531880/pexels-photo-531880.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')`, backgroundPosition:'center', backgroundSize:'cover'}}>
+            <h1 style={{marginTop:'20px'}}>Contact Form</h1>
+            <form className="row" style={{margin: "25px 85px 75px 100px"}} onSubmit={sendEmail}>
+                <label style={{float:'left', color:"#bd084a"}}>Name</label>
+                <input className="form-control" type="text" name="user_name" />
+
+                <label style={{float:'left', color:"#bd084a"}}>Email</label>
+                <input className="form-control" type="email" name="user_email" />
+
+                <label style={{float:'left', color:"#bd084a"}}>Message</label>
+                <textarea className="form-control" name="user_message" rows="4" />
+
+                <input className="form-control btn btn-primary" style={{marginTop:'30px'}} type="submit" value="send" />
+            </form>
+        </div>
+    );
+}
+
+export default mailer;
